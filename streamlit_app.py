a484c57d1c07d7220adf81306df85308e1e3bd4b
ictnmd <ictnmd@gmail.com>
2022-10-22 21:52:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,14 +42,14 @@ with st.echo(code_location='below'):
 
 
 uploaded_file = st.file_uploader("Choose a brain MRI ...", type="jpg")
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded MRI.', use_column_width=True)
-        st.write("")
-        st.write("Classifying...")
-        label = teachable_machine_classification(image, 'keras_model.h5')
-        if label == 0:
-            st.write("The MRI scan has a brain tumor")
-        else:
-            st.write("The MRI scan is healthy")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded MRI.', use_column_width=True)
+    st.write("")
+    st.write("Classifying...")
+    label = teachable_machine_classification(image, 'keras_model.h5')
+    if label == 0:
+        st.write("The MRI scan has a brain tumor")
+    else:
+        st.write("The MRI scan is healthy")
 
