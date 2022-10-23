0733b590174a2a987e86cc3ed7886c3f799c7917
ictnmd <ictnmd@gmail.com>
2022-10-23 11:27:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,42 +24,23 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 st.title("Fruit Classification")
 
+CLASS_LIST = ['Táo','Bơ','Chuối','Nho','Ổi','Chanh','Kiwi','Cam','Đào','Thơm','Dâu tây','Cà chua','Dưa hấu','Chanh dây','Lựu']
 def classify_and_label(image):
     label, score = fruit_classification(image, 'keras_model.h5')
-    if label == 0:
-        st.write("Đây là quả Táo")
-    elif label == 1:
-        st.write("Đây là quả Bơ")
-    elif label == 2:
-        st.write("Đây là quả Chuối")
-    elif label == 3:
-        st.write("Đây là quả Nho")
-    elif label == 4:
-        st.write("Đây là quả Ổi")
-    elif label == 5:
-        st.write("Đây là quả Chanh")
-    elif label == 6:
-        st.write("Đây là quả Kiwi")
-    elif label == 7:
-        st.write("Đây là quả Cam")
-    elif label == 8:
-        st.write("Đây là quả Đào")
-    elif label == 9:
-        st.write("Đây là quả Thơm")
-    elif label == 10:
-        st.write("Đây là quả Dâu tây")
-    elif label == 11:
-        st.write("Đây là quả Cà chua")
-    elif label == 12:
-        st.write("Đây là quả Dưa hấu")
-    elif label == 13:
-        st.write("Đây là quả Chanh dây")
-    elif label == 14:
-        st.write("Đây là quả Lựu")
-    
-    else:
-        st.write("Không rõ")
+    st.write('Đây là quả',CLASS_LIST[label])
     st.write("Với tỷ lệ: ",score)
+    return label, score
+
+
+def write_bytesio_to_file(filename, bytesio):
+    """
+    Write the contents of the given BytesIO to a file.
+    Creates the file or overwrites the file if it does
+    not exist yet. 
+    """
+    with open(filename, "wb") as outfile:
+        # Copy the BytesIO stream to the output file
+        outfile.write(bytesio.getbuffer())
 
 uploaded_file = st.file_uploader("Tải lên hình ảnh", type="jpg")
 if uploaded_file is not None:
@@ -76,10 +57,25 @@ upload_file2=st.file_uploader("Choose a video file", type="mp4")
 if upload_file2 is not None:
     st.video(upload_file2)
 
+    temp_file_to_save = './temp_file_1.mp4'
+    temp_file_result  = './temp_file_2.mp4'
     tfile = tempfile.NamedTemporaryFile(delete=False) 
     tfile.write(upload_file2.read())
 
+
     vf = cv2.VideoCapture(tfile.name)
+
+    # Get the width and height of the video.
+    original_video_width = int(vf.get(cv2.CAP_PROP_FRAME_WIDTH))
+    original_video_height = int(vf.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    frame_fps = vf.get(cv2.CAP_PROP_FPS)
+
+    # # Initialize the VideoWriter Object to store the output video in the disk.
+    # video_writer = cv2.VideoWriter(output_file_path, cv2.VideoWriter_fourcc('M', 'P', '4', 'V'),vf.get(cv2.CAP_PROP_FPS), (original_video_width, original_video_height))
+    
+    # specify a writer to write a processed video to a disk frame by frame
+    fourcc_mp4 = cv2.VideoWriter_fourcc(*'mp4v')
+    out_mp4 = cv2.VideoWriter(temp_file_result, fourcc_mp4, frame_fps, (width, height),isColor = True)
     frames_queue = deque(maxlen = 20)
     stframe = st.empty()
     sec = 0
@@ -102,7 +98,18 @@ if upload_file2 is not None:
             
             resized_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB)
             frames_queue.append(resized_frame)
+            
+
+            label, score = classify_and_label(Image.fromarray(resized_frame))
 
 
-            classify_and_label(Image.fromarray(resized_frame))
-            #here iwant to upload te images
+
+            # Write predicted class name on top of the frame.
+            cv2.putText(frame, CLASS_LIST[label], (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+            # Write The frame into the disk using the VideoWriter Object.
+            out_mp4.write(frame)
+    out_mp4.release()
+    vf.release()
+    st.video(temp_file_result)
+
